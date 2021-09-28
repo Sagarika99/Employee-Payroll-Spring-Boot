8aecaf4f04369b9bb0cb6c56560d0327002dbf83
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-29 02:06:54 +0530

    user friendly error response in case validation fails

--- src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
@@ -1,11 +1,13 @@
 package com.bridgelabz.employeepayroll.dto;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 
 public class EmployeePayrollDTO {
 
 	@Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$", message="Employee name invalid")
 	public String name;
+	@Min(value=500, message="Min Wage should be more than 500")
 	public long salary;	
 	
 	public EmployeePayrollDTO() {}
